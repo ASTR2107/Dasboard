3c7a6d4ecb78591b54990527c3b969238f4464dc
ASTR2107 <aidamir606@mail.ru>
2024-06-28 15:48:14 +0300

    first commit

---
+++ app/src/main/java/WeatherModel/WeatherModel.kt
@@ -0,0 +1,16 @@
+package WeatherModel
+
+data class WeatherModel(
+    val city : String,
+    val time: String,
+    val currentTemp: String,
+    val temp: String,
+    val condition: String,
+    val icon: String,
+    val maxTemp: String,
+    val minTemp: String,
+
+
+
+
+)
---
+++ app/src/main/java/com/example/compousehome/Next.kt
@@ -0,0 +1,16 @@
+package com.example.compousehome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+
+class Next : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+
+        }
+    }
+}
